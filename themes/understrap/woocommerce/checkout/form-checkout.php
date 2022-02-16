ac16f310bc015757849c125880c4109c07583416
Gage Bateman <gbateman@kyanicorp.com>
2022-02-16 13:27:32 -0700

    no message

--- themes/understrap/woocommerce/checkout/form-checkout.php
+++ themes/understrap/woocommerce/checkout/form-checkout.php
@@ -15,136 +15,53 @@
  * @version 3.5.0
  */
 
-defined( 'ABSPATH' ) || exit;
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
 
-/*----------------------------------------------------------------------*/
-/* [ Custom Theme output ]
-/*----------------------------------------------------------------------*/
-if ( true === wpex_has_woo_mods() ) :
+do_action( 'woocommerce_before_checkout_form', $checkout );
 
-	do_action( 'woocommerce_before_checkout_form', $checkout );
+// If checkout registration is disabled and not logged in, the user cannot checkout.
+if ( ! $checkout->is_registration_enabled() && $checkout->is_registration_required() && ! is_user_logged_in() ) {
+	echo esc_html( apply_filters( 'woocommerce_checkout_must_be_logged_in_message', __( 'You must be logged in to checkout.', 'woocommerce' ) ) );
+	return;
+}
 
-	// If checkout registration is disabled and not logged in, the user cannot checkout.
-	if ( ! $checkout->is_registration_enabled() && $checkout->is_registration_required() && ! is_user_logged_in() ) {
-		echo esc_html( apply_filters( 'woocommerce_checkout_must_be_logged_in_message', esc_html__( 'You must be logged in to checkout.', 'total' ) ) );
-		return;
-	}
+?>
 
-	/*** Total checks ***/
-	$or_placement = get_theme_mod( 'woo_checkout_order_review_placement', 'right_col' ); ?>
+<form name="checkout" method="post" class="checkout woocommerce-checkout" action="<?php echo esc_url( wc_get_checkout_url() ); ?>" enctype="multipart/form-data">
 
-	<form name="checkout" method="post" class="checkout woocommerce-checkout" action="<?php echo esc_url( wc_get_checkout_url() ); ?>" enctype="multipart/form-data">
+	<?php if ( $checkout->get_checkout_fields() ) : ?>
 
-		<?php if ( $checkout->get_checkout_fields() ) : ?>
+		<?php do_action( 'woocommerce_checkout_before_customer_details' ); ?>
 
-			<?php do_action( 'woocommerce_checkout_before_customer_details' ); ?>
-
-			<div class="col2-set" id="customer_details">
-
-				<div class="col-1">
-					<?php do_shortcode( 'woocommerce_checkout_billing' ); ?>
-				</div>
-
-				<div class="col-2">
-
-					<?php do_action( 'woocommerce_checkout_shipping' ); ?>
-
-					<?php if ( 'right_col' == $or_placement || empty( $or_placement ) ) { ?>
-
-						<?php do_action( 'woocommerce_checkout_before_order_review_heading' ); ?>
-
-						<h3 id="order_review_heading"><?php esc_html_e( 'Your order', 'woocommerce' ); ?></h3>
-
-						<?php do_action( 'woocommerce_checkout_before_order_review' ); ?>
-
-						<div id="order_review" class="woocommerce-checkout-review-order">
-							<?php do_action( 'woocommerce_checkout_order_review' ); ?>
-						</div>
-
-						<?php do_action( 'woocommerce_checkout_after_order_review' ); ?>
-
-					<?php } ?>
-
-				</div>
-
+		<div class="row" id="customer_details">
+			<div class="col-12 col-sm-7">
+				<?php do_action( 'woocommerce_checkout_billing' ); ?>
 			</div>
 
-			<?php do_action( 'woocommerce_checkout_after_customer_details' ); ?>
-
-		<?php endif; ?>
-
-		<?php
-		// This is the default WooCommerce placement.
-		if ( 'left_col' == $or_placement ) { ?>
-
-			<?php do_action( 'woocommerce_checkout_before_order_review_heading' ); ?>
-
-			<h3 id="order_review_heading"><?php esc_html_e( 'Your order', 'woocommerce' ); ?></h3>
-
-			<?php do_action( 'woocommerce_checkout_before_order_review' ); ?>
-
-			<div id="order_review" class="woocommerce-checkout-review-order">
-				<?php do_action( 'woocommerce_checkout_order_review' ); ?>
+			<div class="col-12 col-sm-5">
+				<?php do_action( 'woocommerce_checkout_shipping' ); ?>
 			</div>
-
-			<?php do_action( 'woocommerce_checkout_after_order_review' ); ?>
-
-		<?php } ?>
-
-	</form>
-
-	<?php do_action( 'woocommerce_after_checkout_form', $checkout ); ?>
-
-<?php
-/*----------------------------------------------------------------------*/
-/* [ Default output ]
-/*----------------------------------------------------------------------*/
-else :
-
-	do_action( 'woocommerce_before_checkout_form', $checkout );
-
-	// If checkout registration is disabled and not logged in, the user cannot checkout.
-	if ( ! $checkout->is_registration_enabled() && $checkout->is_registration_required() && ! is_user_logged_in() ) {
-		echo esc_html( apply_filters( 'woocommerce_checkout_must_be_logged_in_message', __( 'You must be logged in to checkout.', 'woocommerce' ) ) );
-		return;
-	}
-
-	?>
-
-	<form name="checkout" method="post" class="checkout woocommerce-checkout" action="<?php echo esc_url( wc_get_checkout_url() ); ?>" enctype="multipart/form-data">
-
-		<?php if ( $checkout->get_checkout_fields() ) : ?>
-
-			<?php do_action( 'woocommerce_checkout_before_customer_details' ); ?>
-
-			<div class="col2-set" id="customer_details">
-				<div class="col-1">
-					<?php do_action( 'woocommerce_checkout_billing' ); ?>
-				</div>
-
-				<div class="col-2">
-					<?php do_action( 'woocommerce_checkout_shipping' ); ?>
-				</div>
-			</div>
-
-			<?php do_action( 'woocommerce_checkout_after_customer_details' ); ?>
-
-		<?php endif; ?>
-
-		<?php do_action( 'woocommerce_checkout_before_order_review_heading' ); ?>
-
-		<h3 id="order_review_heading"><?php esc_html_e( 'Your order', 'woocommerce' ); ?></h3>
-
-		<?php do_action( 'woocommerce_checkout_before_order_review' ); ?>
-
-		<div id="order_review" class="woocommerce-checkout-review-order">
-			<?php do_action( 'woocommerce_checkout_order_review' ); ?>
 		</div>
 
-		<?php do_action( 'woocommerce_checkout_after_order_review' ); ?>
+		<?php do_action( 'woocommerce_checkout_after_customer_details' ); ?>
 
-	</form>
+	<?php endif; ?>
 
-	<?php do_action( 'woocommerce_after_checkout_form', $checkout ); ?>
+	<?php do_action( 'woocommerce_checkout_before_order_review_heading' ); ?>
 
-<?php endif; ?>
+	<h3 id="order_review_heading"><?php esc_html_e( 'Your order', 'woocommerce' ); ?></h3>
+
+	<?php do_action( 'woocommerce_checkout_before_order_review' ); ?>
+
+	<div id="order_review" class="woocommerce-checkout-review-order">
+		<?php do_action( 'woocommerce_checkout_order_review' ); ?>
+	</div>
+
+	<?php do_action( 'woocommerce_checkout_after_order_review' ); ?>
+
+</form>
+
+<?php
+do_action( 'woocommerce_after_checkout_form', $checkout );
--- themes/understrap/woocommerce/global/form-login.php
+++ themes/understrap/woocommerce/global/form-login.php
@@ -1,8 +1,8 @@
 <?php
 /**
- * Login Form
+ * Login form
  *
- * This template can be overridden by copying it to yourtheme/woocommerce/myaccount/form-login.php.
+ * This template can be overridden by copying it to yourtheme/woocommerce/global/form-login.php.
  *
  * HOWEVER, on occasion WooCommerce will need to update template files and you
  * (the theme developer) will need to copy the new files to your theme to
@@ -11,70 +11,57 @@
  * the readme will list any important changes.
  *
  * @see     https://docs.woocommerce.com/document/template-structure/
- * @package WooCommerce\Templates
- * @version 4.1.0
+ * @package WooCommerce/Templates
+ * @version 3.6.0
  */
 
-if ( ! defined( 'ABSPATH' ) ) {
-	exit; // Exit if accessed directly.
+// Exit if accessed directly.
+defined( 'ABSPATH' ) || exit;
+
+
+
+
+
+
+
+if ( is_user_logged_in() ) {
+	return;
 }
 
-do_action( 'woocommerce_before_customer_login_form' ); ?>
+?>
+<form class="woocommerce-form woocommerce-form-login login" method="post" <?php echo ( $hidden ) ? 'style="display:none;"' : ''; ?>>
 
-<?php if ( 'yes' === get_option( 'woocommerce_enable_myaccount_registration' ) ) : ?>
+	<?php do_action( 'woocommerce_login_form_start' ); ?>
 
-<div class="u-columns col2-set" id="customer_login">
+	<?php echo ( $message ) ? wpautop( wptexturize( $message ) ) : ''; // @codingStandardsIgnoreLine ?>
 
-	<div class="u-column1 col-1">
+	<p class="form-row form-row-first">
+		<label for="username"><?php esc_html_e( 'Username or email', 'woocommerce' ); ?>&nbsp;<span class="required">*</span></label>
+		<input type="text" class="input-text form-control" name="username" id="username" autocomplete="username" />
+	</p>
+	<p class="form-row form-row-last">
+		<label for="password"><?php esc_html_e( 'Password', 'woocommerce' ); ?>&nbsp;<span class="required">*</span></label>
+		<input class="input-text form-control" type="password" name="password" id="password" autocomplete="current-password" />
+	</p>
+	<div class="clear"></div>
 
-		<?php endif; ?>
+	<?php do_action( 'woocommerce_login_form' ); ?>
 
-		<h2><?php esc_html_e( 'Login', 'woocommerce' ); ?></h2>
+	<p class="form-row">
+		<label class="woocommerce-form__label woocommerce-form__label-for-checkbox woocommerce-form-login__rememberme">
+			<input class="woocommerce-form__input woocommerce-form__input-checkbox" name="rememberme" type="checkbox" id="rememberme" value="forever" /> <span><?php esc_html_e( 'Remember me', 'woocommerce' ); ?></span>
+		</label>
+		<?php wp_nonce_field( 'woocommerce-login', 'woocommerce-login-nonce' ); ?>
+		<input type="hidden" name="redirect" value="<?php echo esc_url( $redirect ); ?>" />
+		<button type="submit" class="btn btn-outline-primary" name="login" value="<?php esc_attr_e( 'Login', 'woocommerce' ); ?>"><?php esc_html_e( 'Login', 'woocommerce' ); ?></button>
 
-		<form class="woocommerce-form woocommerce-form-login login" method="post">
+	</p>
+	<p class="lost_password">
+		<a href="<?php echo esc_url( wp_lostpassword_url() ); ?>"><?php esc_html_e( 'Lost your password?', 'woocommerce' ); ?></a>
+	</p>
 
-			<?php do_action( 'woocommerce_login_form_start' ); ?>
+	<div class="clear"></div>
 
-			<p class="woocommerce-form-row woocommerce-form-row--wide form-row form-row-wide">
-				<label for="username"><?php esc_html_e( 'Username or email address', 'woocommerce' ); ?>&nbsp;<span class="required">*</span></label>
-				<input type="text" class="woocommerce-Input woocommerce-Input--text input-text" name="username" id="username" autocomplete="username" value="<?php echo ( ! empty( $_POST['username'] ) ) ? esc_attr( wp_unslash( $_POST['username'] ) ) : ''; ?>" /><?php // @codingStandardsIgnoreLine ?>
-			</p>
-			<p class="woocommerce-form-row woocommerce-form-row--wide form-row form-row-wide">
-				<label for="password"><?php esc_html_e( 'Password', 'woocommerce' ); ?>&nbsp;<span class="required">*</span></label>
-				<input class="woocommerce-Input woocommerce-Input--text input-text" type="password" name="password" id="password" autocomplete="current-password" />
-			</p>
+	<?php do_action( 'woocommerce_login_form_end' ); ?>
 
-			<?php do_action( 'woocommerce_login_form' ); ?>
-
-			<p class="form-row">
-				<label class="woocommerce-form__label woocommerce-form__label-for-checkbox woocommerce-form-login__rememberme">
-					<input class="woocommerce-form__input woocommerce-form__input-checkbox" name="rememberme" type="checkbox" id="rememberme" value="forever" /> <span><?php esc_html_e( 'Remember me', 'woocommerce' ); ?></span>
-				</label>
-				<?php wp_nonce_field( 'woocommerce-login', 'woocommerce-login-nonce' ); ?>
-				<button type="submit" class="woocommerce-button button woocommerce-form-login__submit" name="login" value="<?php esc_attr_e( 'Log in', 'woocommerce' ); ?>"><?php esc_html_e( 'Log in', 'woocommerce' ); ?></button>
-			</p>
-			<p class="woocommerce-LostPassword lost_password">
-				<a href="<?php echo esc_url( wp_lostpassword_url() ); ?>"><?php esc_html_e( 'Lost your password?', 'woocommerce' ); ?></a>
-			</p>
-
-			<?php do_action( 'woocommerce_login_form_end' ); ?>
-
-		</form>
-
-		<?php if ( 'yes' === get_option( 'woocommerce_enable_myaccount_registration' ) ) : ?>
-
-	</div>
-
-	<div class="u-column2 col-2">
-
-		<h2><?php esc_html_e( 'Register', 'woocommerce' ); ?></h2>
-
-		<?php echo(do_shortcode('[ultimatemember form_id="5"]')) ?>
-
-
-	</div>
-
-</div>
-<?php endif; ?>
-
-<?php do_action( 'woocommerce_after_customer_login_form' ); ?>
+</form>
