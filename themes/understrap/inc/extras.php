c6c49511b51c3b4fdd570aa24b5412bb15a249bd
Gage Bateman <gbateman@kyanicorp.com>
2022-02-22 12:27:06 -0700

    no message

--- themes/understrap/inc/extras.php
+++ themes/understrap/inc/extras.php
@@ -292,3 +292,26 @@ function custom_quantity_fields_script(){
 	</script>
 	<?php
 }
+
+
+/**
+ * @snippet       Calculate Subtotal Based on Quantity - WooCommerce Single Product
+ * @author        Codeithub
+ */
+
+add_action( 'woocommerce_after_add_to_cart_button', 'Codeithub_product_price_recalculate' );
+
+function codeithub_product_price_recalculate() {
+	global $product;
+	echo '<div style="display:inline-block;">Total: <span></span></div>';
+	$price = $product->get_price();
+	$currency = get_woocommerce_currency_symbol();
+	wc_enqueue_js( "
+      $('[name=quantity]').on('input change', function() {
+         var qty = $(this).val();
+         var price = '" . esc_js( $price ) . "';
+         var price_string = (price*qty).toFixed(2);
+         $('#subtot > span').html('" . esc_js( $currency ) . "'+price_string);
+      }).change();
+   " );
+}
