95368a9ddec2d5d0ac58e8407455aa00cdc17a78
Gage Bateman <gbateman@kyanicorp.com>
2022-02-25 11:56:52 -0700

    no message

--- plugins/kyani-custom-plugin/includes/replicatedDisplay/replicatedDisplay.php
+++ plugins/kyani-custom-plugin/includes/replicatedDisplay/replicatedDisplay.php
@@ -26,7 +26,7 @@ function replicated_display_shortcode()
 	// IF rep and rep found create menu item
 	if ($rep) {
 		if ($rep->rep_found()) {
-			$replicated_display = '<li class="nav-item"><a class="repDropdown nav-link" onclick="toggleRepDisplay()">You\'re Shopping With ' . $rep->get_rep_name() . '</a></li>';
+			$replicated_display = '<li class="nav-item"><a class="repDropdown nav-link" onclick="toggleRepDisplay()">You\'re Shopping With <span>' . $rep->get_rep_name() . '</span></a></li>';
 
 			$replicated_display .= '<div id="repDisplay" style="display: none;" class="col-12"><div class="col-7 row d-flex justify-content-center p-5 desktop-only">
 								  <div class="col-2 col-md-5 col-lg-2 col-xl-2 p-3 text-right"><img src="' . $rep->get_rep_image() . '" class="repImage"></div>
