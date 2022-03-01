8641c2a5bcda587b86cd223dd7e561c6f2b92d41
Gage Bateman <gbateman@kyanicorp.com>
2022-03-01 09:38:16 -0700

    no message

--- themes/Total/woocommerce/checkout/form-checkout.php
+++ themes/Total/woocommerce/checkout/form-checkout.php
@@ -42,7 +42,7 @@ if ( true === wpex_has_woo_mods() ) :
 			<div class="col2-set" id="customer_details">
 
 				<div class="col-1">
-					<?php do_shortcode( 'woocommerce_checkout_billing' ); ?>
+					<?php do_action( 'woocommerce_checkout_billing' ); ?>
 				</div>
 
 				<div class="col-2">
