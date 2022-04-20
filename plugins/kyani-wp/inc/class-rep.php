77d9e4b9cea32aa4a970dae784e8339b21e6ae11
Gage Bateman <gbateman@kyanicorp.com>
2022-04-20 12:02:30 -0600

    no message

--- plugins/kyani-wp/inc/class-rep.php
+++ plugins/kyani-wp/inc/class-rep.php
@@ -123,19 +123,9 @@ function add_rep_query_var($link)
 		$path = str_replace('https/', '', $uri);
 		$pathfin = substr_replace($path, $rep . '.nitronutritionlife.com/', 0, 0);
 		return 'https://' . $pathfin;
-	} else if (isset($_SERVER['HTTP_X_KYANI_REP']) && strpos($link, "wp-admin") === TRUE) {
-		echo 'wp-admin';
-		$rep = explode(';', $_SERVER['HTTP_X_KYANI_REP'])[0];
-		$uri = str_replace($_SERVER['HTTP_X_FORWARDED_PROTO'] . "://" . $_SERVER['HTTP_HOST'], "", $link);
-		$path = str_replace('https/', '', $uri);
-		$pathfin = substr_replace($path, $rep . '.nitronutritionlife.com/', 0, 0);
-		return 'https://' . $pathfin;
 	} else if (!isset($_SERVER['HTTP_X_KYANI_REP'])) {
 		echo 'nrom';
-		$uri = str_replace($_SERVER['HTTP_X_FORWARDED_PROTO'] . "://" . $_SERVER['HTTP_HOST'], "", $link);
-		$path = str_replace('https/', '', $uri);
-		$pathfin = substr_replace($path, 'nitronutritionlife.com/', 0, 0);
-		return 'https://' . $pathfin;
+		return $link;
 	}
 	return $link;
 }
