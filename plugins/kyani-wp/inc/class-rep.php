0e1a82ee86686318fd598b6627fb240e98c86b85
Gage Bateman <gbateman@kyanicorp.com>
2022-04-20 11:59:45 -0600

    no message

--- plugins/kyani-wp/inc/class-rep.php
+++ plugins/kyani-wp/inc/class-rep.php
@@ -117,12 +117,21 @@ add_action('init', 'set_rep');
 function add_rep_query_var($link)
 {
 	if (isset($_SERVER['HTTP_X_KYANI_REP'])) {
+		echo 'rep';
+		$rep = explode(';', $_SERVER['HTTP_X_KYANI_REP'])[0];
+		$uri = str_replace($_SERVER['HTTP_X_FORWARDED_PROTO'] . "://" . $_SERVER['HTTP_HOST'], "", $link);
+		$path = str_replace('https/', '', $uri);
+		$pathfin = substr_replace($path, $rep . '.nitronutritionlife.com/', 0, 0);
+		return 'https://' . $pathfin;
+	} else if (isset($_SERVER['HTTP_X_KYANI_REP']) && strpos($link, "wp-admin") === TRUE) {
+		echo 'wp-admin';
 		$rep = explode(';', $_SERVER['HTTP_X_KYANI_REP'])[0];
 		$uri = str_replace($_SERVER['HTTP_X_FORWARDED_PROTO'] . "://" . $_SERVER['HTTP_HOST'], "", $link);
 		$path = str_replace('https/', '', $uri);
 		$pathfin = substr_replace($path, $rep . '.nitronutritionlife.com/', 0, 0);
 		return 'https://' . $pathfin;
 	} else if (!isset($_SERVER['HTTP_X_KYANI_REP'])) {
+		echo 'nrom';
 		$uri = str_replace($_SERVER['HTTP_X_FORWARDED_PROTO'] . "://" . $_SERVER['HTTP_HOST'], "", $link);
 		$path = str_replace('https/', '', $uri);
 		$pathfin = substr_replace($path, 'nitronutritionlife.com/', 0, 0);
