800511ca6df6b2326c08601851b51fa148de8da0
Gage Bateman <gbateman@kyanicorp.com>
2022-04-20 14:02:57 -0600

    no message

--- themes/understrap/inc/woocommerce.php
+++ themes/understrap/inc/woocommerce.php
@@ -61,20 +61,6 @@ function wooc_extra_register_fields()
 	<?php
 }
 
-add_action('woocommerce_register_form_end', 'wooc_extra_register_fields');
-
-function woocc_extra_register_fields()
-{
-	global $wp_roles; ?>
-	<script>    if (document.cookie.indexOf('role') > -1) {
-			document.getElementById("billing_role").value = "um_health-pro";
-
-		}</script>
-	<?php
-}
-
-add_action('init', 'woocc_extra_register_fields');
-
 //saving role
 add_action('woocommerce_created_customer', 'update_user_role');
 function update_user_role($user_id)
