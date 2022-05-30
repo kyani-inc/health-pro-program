df50b0a208af412b79b6e9ecd2890ed34fcb9b25
Gage Bateman <gbateman@kyanicorp.com>
2022-05-30 15:45:57 -0600

    no message

--- plugins/kyani-wp/inc/replicated-display/assets/script.js
+++ plugins/kyani-wp/inc/replicated-display/assets/script.js
@@ -1,9 +1,15 @@
 function toggleRepDisplay() {
   let bio = document.getElementById("repDisplay");
+  let content = document.getElementById("main");
+  let newHeight = document.getElementById("repDisplay").clientHeight;
   if (bio.style.display === "none") {
     bio.style.display = "flex";
+    newHeight = document.getElementById("repDisplay").clientHeight;
+    content.style.marginTop = newHeight + "px";
   } else {
     bio.style.display = "none";
+    newHeight = document.getElementById("repDisplay").clientHeight;
+    content.style.marginTop = newHeight + "px";
   }
 }
 
@@ -15,10 +21,14 @@ function toggleBio() {
     bio.style.display = "block";
     show.style.display = "none";
     hide.style.display = "block";
+    newHeight = document.getElementById("repDisplay").clientHeight;
+    content.style.marginTop = newHeight + "px";
   } else {
     bio.style.display = "none";
     show.style.display = "block";
     hide.style.display = "none";
+    newHeight = document.getElementById("repDisplay").clientHeight;
+    content.style.marginTop = newHeight + "px";
   }
 }
 
