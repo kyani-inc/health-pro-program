dc31b25e38054a7a4396816ced1b75be5e9baead
GageBateman <gbateman@kyanicorp.com>
2022-08-15 12:16:13 -0600

    Update email-header.php

--- themes/understrap/woocommerce/emails/email-header.php
+++ themes/understrap/woocommerce/emails/email-header.php
@@ -45,7 +45,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 									<table border="0" cellpadding="0" cellspacing="0" width="100%" id="template_header">
 										<tr>
 											<td id="header_wrapper">
-												<h1><?php echo $email_heading; ?></h1>
+												<h1 style="color: #fff;"><?php echo $email_heading; ?></h1>
 											</td>
 										</tr>
 									</table>
